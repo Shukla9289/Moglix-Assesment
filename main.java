9b89669b68b75f23097f226c6a199faea3e50414
Shukla9289 <Satyam.personal123@gmail.com>
2026-07-21 21:15:55 +0530

    Solution of problem statement

--- main.java
+++ main.java
@@ -1,1 +1,35 @@
+import java.util.*;
 
+public class main {
+
+    public static int longestParentheses(String s) {
+        int maxLen = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(i);
+            } 
+            else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i); 
+                } 
+                else {
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestParentheses("(()"));  
+        System.out.println(longestParentheses(")()())")); 
+        System.out.println(longestParentheses(""));       
+    }
+}
